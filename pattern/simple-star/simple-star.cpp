f43b3937ea951936c1896a5473dec1fc9c66f53d
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-12-17 00:33:39 +0530

    add simple star pattern

---
+++ pattern/simple-star/simple-star.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n; 
+
+    int i = 0;
+
+    while(i < n) {
+        int j = 0;
+        while(j < n) {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+} 
