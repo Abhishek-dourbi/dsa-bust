fbb59e1aaa32090d53ca8dd3aa12feb8e463f1d8
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-12-22 22:05:40 +0530

    add number pattern 5

---
+++ pattern/number-pattern-5/index.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n; 
+
+    int i = 1;
+
+    while(i <= n) {
+        int j = 1;
+        while(j <= i) {
+            cout << i << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+} 
+
+
+/*
+    n = 4
+
+    1
+    2 2
+    3 3 3 3
+    4 4 4 4
+*/
