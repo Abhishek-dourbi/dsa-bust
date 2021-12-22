2806b19807282ae15c5c48ed94b014a1732865e5
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-12-22 23:00:10 +0530

    add alphabet pattern 6

---
+++ pattern/alphabet-patterns/alphabet-pattern-6/index.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n; 
+
+    int i = 1;
+    char count = 'A';
+
+    while(i <= n) {
+        int j = 1;
+        while(j <= i) {
+            cout << count << " ";
+            count++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+} 
+
+
+/*
+    A
+    B C
+    D E F
+    G H I J
+*/
