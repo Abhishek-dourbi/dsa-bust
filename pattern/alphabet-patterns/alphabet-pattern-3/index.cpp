76a4a5e3507ba5ddb84cbab1e5576955a8b92a0e
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-12-22 22:50:17 +0530

    add alphabet pattern 3

---
+++ pattern/alphabet-patterns/alphabet-pattern-3/index.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n; 
+
+    int i = 1;
+    char count = 'A';
+
+    while(i <= n) {
+        int j = 1;
+        while(j <= n) {
+            cout << count << " ";
+            count++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+} 
+
+
+/*
+    A B C
+    D E F
+    G H I
+*/
