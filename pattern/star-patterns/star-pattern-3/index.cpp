114247f0ee654e15d9e135de56fced4f95b0a2c8
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-12-23 22:47:05 +0530

    add star pattern 3

---
+++ pattern/star-patterns/star-pattern-3/index.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n; 
+
+    int i = 1;
+
+    while(i <= n) {
+        int j = 1;
+        while(j <= n) {
+            if(j > n - i) {
+                cout << '*';
+            } else {
+                cout << ' ';
+            }
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+} 
+
+
+/*
+    n = 4
+
+       *
+      **
+     ***
+    ****  
+*/
