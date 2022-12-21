a16a723d5dacd68536f64669547834a995bb4f96
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-12-21 20:34:28 +0530

    peak index in mountain array

---
+++ algorithm/search/binarySearch/problems/leetcode/peak-index-in-mountain-arr/index.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid;
+
+    while(start < end) {
+        mid = start + (end - start) / 2;
+
+        if(arr[mid] < arr[mid + 1]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+
+    return start;
+}
+
+int main() {
+    int arr[4] = {0, 10, 5, 2};
+
+    vector<int> vect(begin(arr), end(arr));
+
+    cout << peakIndexInMountainArray(vect);
+
+    return 0;
+}
