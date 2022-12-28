a5f47b3256e4e3ef1ce4cb04a97a29dbbdf5b0f6
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-12-29 00:02:07 +0530

    search in a rotated array
    alter apporach with no get pivot method

--- algorithm/search/binarySearch/problems/coding-ninjas/search-in-rotated-array/index.cpp
+++ algorithm/search/binarySearch/problems/coding-ninjas/search-in-rotated-array/index.cpp
@@ -50,6 +50,37 @@ int findPosition(vector<int>& arr, int n, int k)
     return -1;
 }
 
+
+// Alter approach with no get pivot
+// int findPosition(vector<int>& nums, int n, int target)
+// {
+//     int start = 0, end = n-1, mid;
+//     while(start <= end){
+//         mid = start + (end - start) / 2;
+        
+//         if(target == nums[mid]){
+//             return mid;
+//         }
+//         if(nums[end] > nums[mid]){
+//             if(nums[mid] < target && nums[end] >= target){
+//                 start = mid+1;
+//             }
+//             else{
+//                 end = mid -1;
+//             }
+//         }
+//         else{
+//             if(nums[mid] > target && nums[start] <= target){
+//                 end = mid-1;
+//             }
+//             else{
+//                 start = mid+1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
 int main() {
     int arr[7] = {2, 4, 5, 6, 8, 9, 1};
 
