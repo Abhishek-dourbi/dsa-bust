801f61858976b976ba7db28245cee43b1389be8c
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-12-30 22:26:16 +0530

    find square root
    get more precise answer upto 3 decimal places

--- algorithm/search/binarySearch/problems/leetcode/sqrt/index.cpp
+++ algorithm/search/binarySearch/problems/leetcode/sqrt/index.cpp
@@ -20,8 +20,28 @@ int mySqrt(int x) {
     return start - 1;
 }
 
+float getPricision(int x, int precision, int ans) {
+    float factor = 1;
+    float res = ans;
+
+    for(int i = 0; i < precision; i++) {
+        factor = factor / 10;
+        float temp = res + factor;
+
+        while((temp * temp) <= x) {
+            temp = temp + factor;
+        }
+        res = temp - factor;
+    }
+
+    return res;
+}
+
 int main() {
-    cout << mySqrt(27);
+    int ans = mySqrt(27);
+    float preciseAns = getPricision(27, 3, ans);
+
+    cout << preciseAns;
 
     return 0;
 }
